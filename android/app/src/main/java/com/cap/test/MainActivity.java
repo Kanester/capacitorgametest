bf57fb4580d3d5dc55f9fad0f3ff9e10e281900c
KanesterP <pampagkieven091@gmail.com>
2025-05-23 18:07:32 +0800

    --no-message

---
+++ android/app/src/main/java/com/cap/test/MainActivity.java
@@ -0,0 +1,5 @@
+package com.cap.test;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.cap.test',
+  appName: 'capTest',
+  webDir: 'build'
+};
+
+export default config;
